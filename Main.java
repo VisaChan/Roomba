d46e603bf353a80cbbcaa11e60f7fd361d3241fc
VisaChan <91771783+VisaChan@users.noreply.github.com>
2021-10-30 22:58:36 -0500

    InitialTest

---
+++ Main.java
@@ -0,0 +1,27 @@
+package GsonDemo5;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
+        //Floor f = new Floor("Demo10-10/trap.json");
+
+        //Floor f = new Floor("Demo10-10/longjohn.json");
+        //Floor f = new Floor("Demo10-10/floor1.json");
+        Floor f = new Floor("Demo10-10/stairDemo.json");
+        CleanSweep c = new CleanSweep(f);
+        Logger log = Logger.getInstance();
+        //activate the clean sweeps
+        c.run();
+
+        System.out.println("Ending tile of CleanSweep run: " + Arrays.toString(c.getCurrentTile()));
+        log.write("Ending tile of CleanSweep run: " + Arrays.toString(c.getCurrentTile()));
+
+
+        log.createLog();
+    }
+
+}
